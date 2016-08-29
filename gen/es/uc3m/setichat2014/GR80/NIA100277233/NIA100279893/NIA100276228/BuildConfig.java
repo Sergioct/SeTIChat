f8a57ce87418c291c5499aaa4d2399b50adfb9ef
SergioCT <SergioCrespoToubes@gmail.com>
2016-08-29 22:57:49 +0200

    SetiChat University work 2014

---
+++ gen/es/uc3m/setichat2014/GR80/NIA100277233/NIA100279893/NIA100276228/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package es.uc3m.setichat2014.GR80.NIA100277233.NIA100279893.NIA100276228;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
